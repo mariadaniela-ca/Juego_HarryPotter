f65762d09a4f66a197b31f8a6bed426f3531a4b8
Maca <53454834+macarena-escalante@users.noreply.github.com>
2019-08-28 18:05:41 -0300

    Actualización de selección de hechizos

--- Harry_Potter_Juego/src/app/App.java
+++ Harry_Potter_Juego/src/app/App.java
@@ -30,8 +30,6 @@ public class App {
 
         juegoNuevo.seleccionarTipoPersonaje(tipoPersonaje);
 
-        System.out.println("    Escoge el numero de tu personaje");
-
         numeroPersonaje = Teclado.nextInt();
 
         Personaje nuevoP = juegoNuevo.crearPersonaje(numeroPersonaje);
@@ -50,20 +48,47 @@ public class App {
 
         System.out.println("    Tu oponente es: " + oponenteP.nombre);
 
-        Hechizo hechizos = new Hechizo();
-        hechizos.cargarHechizos();
-
-
-
-
-
-
         if (nuevoP instanceof IHacerMagia) {
 
-            IHacerMagia nuevoSerMagico = ((IHacerMagia)nuevoP);
+            IHacerMagia nuevoSerMagico = ((IHacerMagia) nuevoP);
+
             System.out.println("Es hora de aprender hechizos!");
-                   
-            
+            Hechizo hechizos = new Hechizo();
+            hechizos.cargarHechizos();
+
+            System.out.println("    Tenemos 4 tipos de hechizos: 1 de Ataque 2 de Curación 3 de Defensa 4 de Ocio");
+            System.out.println(" Escoge que tipo de hechizos quieres (solo puedes 3)");
+
+            int tipoHechizo1;
+            tipoHechizo1 = Teclado.nextInt();
+            hechizos.elegirTipoHechizo(tipoHechizo1);
+            int hechizoElegido1;
+            hechizoElegido1 = Teclado.nextInt();
+            hechizos.elegirHechizo(hechizoElegido1);
+
+            System.out.println("Escoge tu segundo tipo de hechizo: ");
+
+            int tipoHechizo2;
+            tipoHechizo2 = Teclado.nextInt();
+            hechizos.elegirTipoHechizo(tipoHechizo2);
+            int hechizoElegido2;
+            hechizoElegido2 = Teclado.nextInt();
+            hechizos.elegirHechizo(hechizoElegido2);
+
+            System.out.println("Solo un tipo más!");
+
+            int tipoHechizo3;
+            tipoHechizo3 = Teclado.nextInt();
+            hechizos.elegirTipoHechizo(tipoHechizo3);
+            int hechizoElegido3;
+            hechizoElegido3 = Teclado.nextInt();
+            hechizos.elegirHechizo(hechizoElegido3);
+
+            System.out.println("Tu personaje es " + nuevoP.nombre + " y sus hechizos serán " + hechizoElegido1 + ", "
+                    + hechizoElegido2 + ", " + hechizoElegido3);
+
+            // hechizos.buscarDeAtaque();
+
             // for (int i = nuevoP.salud; i >= 0; i--) {}
 
         } else {
--- Harry_Potter_Juego/src/app/poderes/hechizos/Hechizo.java
+++ Harry_Potter_Juego/src/app/poderes/hechizos/Hechizo.java
@@ -5,31 +5,35 @@ import java.util.List;
 
 import app.poderes.Poder;
 import app.poderes.hechizos.hechizosataque.Cruciatus;
+import app.poderes.hechizos.hechizosataque.HechizoAtaque;
 import app.poderes.hechizos.hechizosataque.PetrificusTotalus;
 import app.poderes.hechizos.hechizosataque.Sectumsempra;
 import app.poderes.hechizos.hechizoscuracion.BrackiumEmendo;
+import app.poderes.hechizos.hechizoscuracion.HechizoCuracion;
+import app.poderes.hechizos.hechizoscuracion.Reparifors;
 import app.poderes.hechizos.hechizoscuracion.VulneraSanentur;
 import app.poderes.hechizos.hechizosdefensa.CaveInimicum;
 import app.poderes.hechizos.hechizosdefensa.ExpectoPatronum;
+import app.poderes.hechizos.hechizosdefensa.HechizoDefensa;
 import app.poderes.hechizos.hechizosdefensa.Protego;
+import app.poderes.hechizos.hechizosocio.Accio;
+import app.poderes.hechizos.hechizosocio.Bauleo;
+import app.poderes.hechizos.hechizosocio.HechizoOcio;
+import app.poderes.hechizos.hechizosocio.WingardiumLeviosa;
 
 /**
  * Hechizo
  */
 public class Hechizo extends Poder {
- 
+
     public String nombre;
     public boolean esOscuro;
     public int nivelDeDaño;
     public int nivelDeCuracion;
     public int nivelDeProteccion;
-    public int numeroDeHechizo;
+    public int identificador;
 
-    public static List<Hechizo> hechizosAtaques = new ArrayList<Hechizo>();
-    public static List<Hechizo> hechizosCuracion = new ArrayList<Hechizo>();
-    public static List<Hechizo> hechizosDefensa = new ArrayList<Hechizo>();
-    public static List<Hechizo> hechizosOcio = new ArrayList<Hechizo>();
-
+    public static List<Hechizo> hechizos = new ArrayList<Hechizo>();
 
     public Hechizo(String nombre) {
         this.nombre = nombre;
@@ -38,96 +42,219 @@ public class Hechizo extends Poder {
 
     public Hechizo() {
     }
-    
-	public static <Reparisfors> void cargarHechizos() {
+
+    public void cargarHechizos() {
 
         Cruciatus c = new Cruciatus("Cruciatus");
-        c.numeroDeHechizo = 1;
+        c.identificador = 1;
         c.esOscuro = true;
         c.nivelDeCuracion = 0;
         c.nivelDeDaño = 50;
         c.nivelDeProteccion = 0;
-        hechizosAtaques.add(c);
+        hechizos.add(c);
 
         PetrificusTotalus p = new PetrificusTotalus("Petrificus Totalus");
-        c.numeroDeHechizo = 2;
+        p.identificador = 2;
         p.esOscuro = false;
         p.nivelDeCuracion = 0;
         p.nivelDeDaño = 20;
         p.nivelDeProteccion = 0;
-        hechizosAtaques.add(p);
+        hechizos.add(p);
 
         Sectumsempra s = new Sectumsempra("Sectumsempra");
-        c.numeroDeHechizo = 3;
+        s.identificador = 3;
         s.esOscuro = false;
         s.nivelDeCuracion = 0;
         s.nivelDeDaño = 10;
         s.nivelDeProteccion = 0;
-        hechizosAtaques.add(s);
+        hechizos.add(s);
 
         BrackiumEmendo b = new BrackiumEmendo("Brackium Emendo");
-        c.numeroDeHechizo = 1;
+        b.identificador = 4;
         b.esOscuro = false;
         b.nivelDeCuracion = 20;
         b.nivelDeDaño = 0;
         b.nivelDeProteccion = 0;
-        hechizosCuracion.add(b);
+        hechizos.add(b);
 
-        /*Reparisfors r = new Reparisfors("Refarisfors");
-        c.numeroDeHechizo = 2;
+        Reparifors r = new Reparifors("Reparifors");
+        r.identificador = 5;
         r.esOscuro = false;
-        r.nivelDeCuracion = 15;
+        r.nivelDeCuracion = 25;
         r.nivelDeDaño = 0;
         r.nivelDeProteccion = 0;
-        hechizosCuracion.add(r);
-        */
+        hechizos.add(r);
 
         VulneraSanentur v = new VulneraSanentur("Vulnera Sanentur");
-        c.numeroDeHechizo = 3;
+        v.identificador = 6;
         v.esOscuro = false;
         v.nivelDeCuracion = 20;
         v.nivelDeDaño = 0;
         v.nivelDeProteccion = 0;
-        hechizosCuracion.add(v);
+        hechizos.add(v);
 
         CaveInimicum ci = new CaveInimicum("Cave Inimicum");
-        c.numeroDeHechizo = 1;
+        ci.identificador = 7;
         ci.esOscuro = false;
         ci.nivelDeCuracion = 0;
         ci.nivelDeDaño = 0;
         ci.nivelDeProteccion = 5;
-        hechizosDefensa.add(ci);
+        hechizos.add(ci);
 
         ExpectoPatronum e = new ExpectoPatronum("Expecto Patronum");
-        c.numeroDeHechizo = 2;
+        e.identificador = 8;
         e.esOscuro = false;
         e.nivelDeCuracion = 0;
         e.nivelDeDaño = 0;
         e.nivelDeProteccion = 10;
-        hechizosDefensa.add(e);
+        hechizos.add(e);
 
         Protego pr = new Protego("Protego");
-        c.numeroDeHechizo = 3;
+        pr.identificador = 9;
         pr.esOscuro = false;
         pr.nivelDeCuracion = 0;
         pr.nivelDeDaño = 0;
         pr.nivelDeProteccion = 15;
-        hechizosDefensa.add(pr);
+        hechizos.add(pr);
+
+        Accio acc = new Accio("Accio");
+        acc.identificador = 10;
+        acc.esOscuro = false;
+        acc.nivelDeCuracion = 0;
+        acc.nivelDeDaño = 0;
+        acc.nivelDeProteccion = 0;
+        hechizos.add(acc);
+
+        Bauleo bau = new Bauleo("Bauleo");
+        bau.identificador = 11;
+        bau.esOscuro = false;
+        bau.nivelDeCuracion = 0;
+        bau.nivelDeDaño = 0;
+        bau.nivelDeProteccion = 0;
+        hechizos.add(bau);
+
+        WingardiumLeviosa win = new WingardiumLeviosa("Wingardium Leviosa");
+        win.identificador = 12;
+        win.esOscuro = false;
+        win.nivelDeCuracion = 0;
+        win.nivelDeDaño = 0;
+        win.nivelDeProteccion = 0;
+        hechizos.add(win);
 
     }
 
-    public static void mostrarHechizos() {
-        System.out.println(" Los hechizos de Ataque son: ");
-        for (Hechizo a : hechizosAtaques) {
-            System.out.println( a.numeroDeHechizo + " " + a.nombre + "\n");
-        }
+    {
 
+        /*
+         * for (Hechizo a : hechizos) { System.out.println(a.numeroDeHechizo + " " +
+         * a.nombre + "\n"); }
+         */
+
+    }
+
+    int tipoHechizo1;
+    int tipoHechizo2;
+    int tipoHechizo3;
+
+    int hechizoElegido1;
+    int hechizoElegido2;
+    int hechizoElegido3;
+
+    public void elegirHechizo(int numero) {
+    }
+
+    public void elegirTipoHechizo(int numero) {
+
+        for (int i = tipoHechizo1; i <= tipoHechizo3; i++) {
+
+            switch (numero) {
+            case 1:
+                System.out.println("Indica el número de tu Hechizo de Ataque");
+                List<Hechizo> deAtaque = buscarDeAtaque();
+
+                for (Hechizo a : deAtaque) {
+                    System.out.println(a.identificador + " " + a.nombre);
+                }
+
+                break;
+
+            case 2:
+                System.out.println("Indica el número de tu Hechizo de Curación");
+                List<Hechizo> deCuracion = buscarDeCuracion();
+                for (Hechizo a : deCuracion) {
+                    System.out.println(a.identificador + " " + a.nombre);
+                }
+                break;
+
+            case 3:
+                System.out.println("Indica el número de tu Hechizo de Defensa");
+                List<Hechizo> deDefensa = buscarDeDefensa();
+                for (Hechizo a : deDefensa) {
+                    System.out.println(a.identificador + " " + a.nombre);
+                }
+
+                break;
+
+            case 4:
+                System.out.println("Indica el número de tu Hechizo de Ocio");
+                List<Hechizo> deOcio = buscarDeOcio();
+                for (Hechizo a : deOcio) {
+                    System.out.println(a.identificador + " " + a.nombre);
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+
+    public List<Hechizo> buscarDeAtaque() {
+        List<Hechizo> deAtaque = new ArrayList<>();
+
+        for (Hechizo a : hechizos) {
+            if (a instanceof HechizoAtaque) {
+                deAtaque.add(a);
+            }
+
+        }
+        return deAtaque;
+    }
+
+    public List<Hechizo> buscarDeCuracion() {
+        List<Hechizo> deCuracion = new ArrayList<>();
+
+        for (Hechizo a : hechizos) {
+            if (a instanceof HechizoCuracion) {
+                deCuracion.add(a);
+            }
+
+        }
+        return deCuracion;
+    }
+
+    public List<Hechizo> buscarDeDefensa() {
+        List<Hechizo> deDefensa = new ArrayList<>();
+
+        for (Hechizo a : hechizos) {
+            if (a instanceof HechizoDefensa) {
+                deDefensa.add(a);
+            }
+
+        }
+        return deDefensa;
+    }
+
+    public List<Hechizo> buscarDeOcio() {
+        List<Hechizo> deOcio = new ArrayList<>();
+
+        for (Hechizo a : hechizos) {
+            if (a instanceof HechizoOcio) {
+                deOcio.add(a);
+            }
+
+        }
+        return deOcio;
     }
 
 }
-
-        
-    
-    
-
-
